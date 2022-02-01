ae3b3117f95aec1110ef1580764f41f32506767b
rotte2000217 <rotte2000217@protonmail.com>
2022-02-01 15:36:56 +0200

    Seed the database with data from the 'universities.hipolabs.com' api

--- Universities/Data/UniversitiesDbContext.cs
+++ Universities/Data/UniversitiesDbContext.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Net;
 using Universities.Data.Models;
+using Universities.Models;
 
 namespace Universities.Data
 {
@@ -12,5 +17,35 @@ namespace Universities.Data
         }
 
         public DbSet<UniversityEntity> Universities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var url = $"http://universities.hipolabs.com/search";
+
+            WebRequest request = HttpWebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string responseText = reader.ReadToEnd();
+            UniversityResponseModel[] universityModels = JsonConvert.DeserializeObject<UniversityResponseModel[]>(responseText);
+
+            var id = 1;
+            var universities = universityModels
+                .Select(x => new UniversityEntity
+                {
+                    Id = id++,
+                    Name = x.Name,
+                    AlphaTwoCode = x.AlphaTwoCode,
+                    Country = x.Country,
+                    StateProvince = x.StateProvince,
+                    WebPage = x.WebPages?.FirstOrDefault()
+                })
+                .ToArray();
+
+            builder.Entity<UniversityEntity>().HasData(universities);
+        }
     }
 }
